d7eaa75cc90b1c22dcea0917bfd6d7dc19da6332
keithlem <keithlem9@gmail.com>
2020-02-13 00:42:51 -0500

    Updates

---
+++ precache-manifest.0a7d9392a19e9c59d83f2dc447131d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74af1a44c65a817b43ad",
+    "url": "/reactNotes/static/css/main.07415f89.chunk.css"
+  },
+  {
+    "revision": "74af1a44c65a817b43ad",
+    "url": "/reactNotes/static/js/main.40a9aa79.chunk.js"
+  },
+  {
+    "revision": "0754e245eca17c18e206",
+    "url": "/reactNotes/static/js/runtime~main.751e7945.js"
+  },
+  {
+    "revision": "f821964c0d398c42fbd3",
+    "url": "/reactNotes/static/css/2.bbb21702.chunk.css"
+  },
+  {
+    "revision": "f821964c0d398c42fbd3",
+    "url": "/reactNotes/static/js/2.906c8be8.chunk.js"
+  },
+  {
+    "revision": "32fe59df1e127bd8fc7334284cbb69f3",
+    "url": "/reactNotes/index.html"
+  }
+];

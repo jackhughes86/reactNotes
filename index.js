8a3b1b666731efa8bbba67722b7cede1f93868f7
keithlem <keithlem9@gmail.com>
2020-02-13 01:06:30 -0500

    commit

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+<link rel="manifest" href="%PUBLIC_URL%/manifest.json" />;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
